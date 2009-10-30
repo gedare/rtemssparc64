bd7a9495dfb1a780008a741703bbce010cb21e6c
gedare@gwmail.gwu.edu <gedare@gwmail.gwu.edu@bd9825d0-bf6e-11de-8cd0-f941f5a20a08>
2009-10-30 23:39:43 +0000

    first cut at raw ISR handler, added cache_manager code to build

--- trunk/rtems/rtemscvs/cpukit/score/cpu/sparc64/rtems/sparc64/traps.h
+++ trunk/rtems/rtemscvs/cpukit/score/cpu/sparc64/rtems/sparc64/traps.h
@@ -17,13 +17,13 @@
 
 #define TRAP(_vector, _handler)  \
   rdpr   %pstate, %l0 ; \
-  sethi %hi(_handler), %l4 ; \
+  sethi  %hh(_handler), %l3 ; \
+  or     %l3, %hm(_handler), %l3 ; \
+  sllx   %l3, 32, %l3 ; \
+  sethi  %hi(_handler), %l4 ; \
+  or     %l4, %l3, %l4 ; \
   jmp   %l4+%lo(_handler); \
-  mov   _vector, %l3 ; \
-  nop ; \
-  nop ; \
-  nop ; \
-  nop
+  mov   _vector, %l3 ; 
 
 /*
  *  Used for the reset trap to avoid a supervisor instruction
@@ -35,43 +35,46 @@
   jmp   %l4+%lo(_handler); \
   mov   _vector, %l3
 
-#define L0_OFFSET       0
-#define L1_OFFSET       8
-#define L2_OFFSET       16
-#define L3_OFFSET       24
-#define L4_OFFSET       32
-#define L5_OFFSET       40
-#define L6_OFFSET       48
-#define L7_OFFSET       56
-#define I0_OFFSET       64
-#define I1_OFFSET       72
-#define I2_OFFSET       80
-#define I3_OFFSET       88
-#define I4_OFFSET       96
-#define I5_OFFSET       104
-#define I6_OFFSET       112
-#define I7_OFFSET       120
+/* 
+ * These are defined in cpu.h, but for offset in context_control
+ */
+#define L0_OFFSET_FROM_SP       0
+#define L1_OFFSET_FROM_SP       8
+#define L2_OFFSET_FROM_SP       16
+#define L3_OFFSET_FROM_SP       24
+#define L4_OFFSET_FROM_SP       32
+#define L5_OFFSET_FROM_SP       40
+#define L6_OFFSET_FROM_SP       48
+#define L7_OFFSET_FROM_SP       56
+#define I0_OFFSET_FROM_SP       64
+#define I1_OFFSET_FROM_SP       72
+#define I2_OFFSET_FROM_SP       80
+#define I3_OFFSET_FROM_SP       88
+#define I4_OFFSET_FROM_SP       96
+#define I5_OFFSET_FROM_SP       104
+#define I6_OFFSET_FROM_SP       112
+#define I7_OFFSET_FROM_SP       120
 
 /*
  * Used for register overflow to spill a register window
  */
 .macro SPILL_WINDOW_HANDLER
-	stx %l0, [%sp + STACK_BIAS + L0_OFFSET]	
-	stx %l1, [%sp + STACK_BIAS + L1_OFFSET]
-	stx %l2, [%sp + STACK_BIAS + L2_OFFSET]
-	stx %l3, [%sp + STACK_BIAS + L3_OFFSET]
-	stx %l4, [%sp + STACK_BIAS + L4_OFFSET]
-	stx %l5, [%sp + STACK_BIAS + L5_OFFSET]
-	stx %l6, [%sp + STACK_BIAS + L6_OFFSET]
-	stx %l7, [%sp + STACK_BIAS + L7_OFFSET]
-	stx %i0, [%sp + STACK_BIAS + I0_OFFSET]
-	stx %i1, [%sp + STACK_BIAS + I1_OFFSET]
-	stx %i2, [%sp + STACK_BIAS + I2_OFFSET]
-	stx %i3, [%sp + STACK_BIAS + I3_OFFSET]
-	stx %i4, [%sp + STACK_BIAS + I4_OFFSET]
-	stx %i5, [%sp + STACK_BIAS + I5_OFFSET]
-	stx %i6, [%sp + STACK_BIAS + I6_OFFSET]
-	stx %i7, [%sp + STACK_BIAS + I7_OFFSET]
+	stx %l0, [%sp + STACK_BIAS + L0_OFFSET_FROM_SP]	
+	stx %l1, [%sp + STACK_BIAS + L1_OFFSET_FROM_SP]
+	stx %l2, [%sp + STACK_BIAS + L2_OFFSET_FROM_SP]
+	stx %l3, [%sp + STACK_BIAS + L3_OFFSET_FROM_SP]
+	stx %l4, [%sp + STACK_BIAS + L4_OFFSET_FROM_SP]
+	stx %l5, [%sp + STACK_BIAS + L5_OFFSET_FROM_SP]
+	stx %l6, [%sp + STACK_BIAS + L6_OFFSET_FROM_SP]
+	stx %l7, [%sp + STACK_BIAS + L7_OFFSET_FROM_SP]
+	stx %i0, [%sp + STACK_BIAS + I0_OFFSET_FROM_SP]
+	stx %i1, [%sp + STACK_BIAS + I1_OFFSET_FROM_SP]
+	stx %i2, [%sp + STACK_BIAS + I2_OFFSET_FROM_SP]
+	stx %i3, [%sp + STACK_BIAS + I3_OFFSET_FROM_SP]
+	stx %i4, [%sp + STACK_BIAS + I4_OFFSET_FROM_SP]
+	stx %i5, [%sp + STACK_BIAS + I5_OFFSET_FROM_SP]
+	stx %i6, [%sp + STACK_BIAS + I6_OFFSET_FROM_SP]
+	stx %i7, [%sp + STACK_BIAS + I7_OFFSET_FROM_SP]
 	saved
 	retry
 .endm
@@ -80,22 +83,22 @@
  * Used to service a trap on restore
  */
 .macro FILL_WINDOW_HANDLER
-	ldx [%sp + STACK_BIAS + L0_OFFSET], %l0
-	ldx [%sp + STACK_BIAS + L1_OFFSET], %l1
-	ldx [%sp + STACK_BIAS + L2_OFFSET], %l2
-	ldx [%sp + STACK_BIAS + L3_OFFSET], %l3
-	ldx [%sp + STACK_BIAS + L4_OFFSET], %l4
-	ldx [%sp + STACK_BIAS + L5_OFFSET], %l5
-	ldx [%sp + STACK_BIAS + L6_OFFSET], %l6
-	ldx [%sp + STACK_BIAS + L7_OFFSET], %l7
-	ldx [%sp + STACK_BIAS + I0_OFFSET], %i0
-	ldx [%sp + STACK_BIAS + I1_OFFSET], %i1
-	ldx [%sp + STACK_BIAS + I2_OFFSET], %i2
-	ldx [%sp + STACK_BIAS + I3_OFFSET], %i3
-	ldx [%sp + STACK_BIAS + I4_OFFSET], %i4
-	ldx [%sp + STACK_BIAS + I5_OFFSET], %i5
-	ldx [%sp + STACK_BIAS + I6_OFFSET], %i6
-	ldx [%sp + STACK_BIAS + I7_OFFSET], %i7
+	ldx [%sp + STACK_BIAS + L0_OFFSET_FROM_SP], %l0
+	ldx [%sp + STACK_BIAS + L1_OFFSET_FROM_SP], %l1
+	ldx [%sp + STACK_BIAS + L2_OFFSET_FROM_SP], %l2
+	ldx [%sp + STACK_BIAS + L3_OFFSET_FROM_SP], %l3
+	ldx [%sp + STACK_BIAS + L4_OFFSET_FROM_SP], %l4
+	ldx [%sp + STACK_BIAS + L5_OFFSET_FROM_SP], %l5
+	ldx [%sp + STACK_BIAS + L6_OFFSET_FROM_SP], %l6
+	ldx [%sp + STACK_BIAS + L7_OFFSET_FROM_SP], %l7
+	ldx [%sp + STACK_BIAS + I0_OFFSET_FROM_SP], %i0
+	ldx [%sp + STACK_BIAS + I1_OFFSET_FROM_SP], %i1
+	ldx [%sp + STACK_BIAS + I2_OFFSET_FROM_SP], %i2
+	ldx [%sp + STACK_BIAS + I3_OFFSET_FROM_SP], %i3
+	ldx [%sp + STACK_BIAS + I4_OFFSET_FROM_SP], %i4
+	ldx [%sp + STACK_BIAS + I5_OFFSET_FROM_SP], %i5
+	ldx [%sp + STACK_BIAS + I6_OFFSET_FROM_SP], %i6
+	ldx [%sp + STACK_BIAS + I7_OFFSET_FROM_SP], %i7
 	restored
 	retry
 .endm
