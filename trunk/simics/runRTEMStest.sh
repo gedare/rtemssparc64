6d5e5e9404ce6250f85badf50ccd479d0ea2cb36
gedare@gwmail.gwu.edu <gedare@gwmail.gwu.edu@bd9825d0-bf6e-11de-8cd0-f941f5a20a08>
2009-12-17 04:37:05 +0000

    fixed crash after return from boot_card, added runtests.sh and automate running sp tests

--- trunk/simics/runRTEMStest.sh
+++ trunk/simics/runRTEMStest.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $# -eq 0 ]
+if [[ $# -eq 0 || $# -eq 1 ]]
 then
 echo "Error - parameters missing"
 echo "Syntax : $0 path_to_iso test_name "
-echo "example : $0 /home/eugen/work/rtemssparc64/rtemsboot/image.iso hello "
+echo "example : $0 /svn/rtemssparc64/rtems/boot/image.iso hello "
 exit 1
 fi
 
@@ -14,10 +14,19 @@ TESTNAME=$2
 
 ISOPATH=`echo $ISOPATH | sed 's/\//\\//g'`
 
+rm $TESTNAME.txt
 
 #cat targets/niagara-simple/niagara-simple-RTEMStemplate.simics | sed s/REPLACEDBYSED534ISOSIZE/$ISOSIZE/ | sed s/REPLACEDBYSED534TESTNAME/$TESTNAME/ | sed "s;REPLACEDBYSED534ISOPATH;$ISOPATH;"
 cat targets/niagara-simple/niagara-simple-RTEMStemplate.simics | sed s/REPLACEDBYSED534ISOSIZE/$ISOSIZE/ | sed s/REPLACEDBYSED534TESTNAME/$TESTNAME/ | sed "s;REPLACEDBYSED534ISOPATH;$ISOPATH;" > temp.simics
 
-simics temp.simics
+./simics temp.simics
+if [[ $? -ne 0 ]]
+then
+	echo "***** Exiting with -1 *****"
+	exit -1
+fi
 
 rm temp.simics
+
+cat $TESTNAME.txt | sed '/^\s*$/d' | sed '/END_TOKEN/d' > temp.txt
+mv temp.txt $TESTNAME.txt
