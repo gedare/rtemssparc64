ca0341a6b2afdc954eedaa730222717d406129fe
gedare@gwmail.gwu.edu <gedare@gwmail.gwu.edu@bd9825d0-bf6e-11de-8cd0-f941f5a20a08>
2010-05-13 22:53:02 +0000

    sun4u hello world still works, need to deal with mmu

--- rtems/build-b-sun4ucvs.sh
+++ rtems/build-b-sun4ucvs.sh
@@ -4,8 +4,8 @@ cd b-sun4ucvs
 rm * -rf
 #../rtemscvs/configure --target=sparc64-rtems4.10 --disable-posix --disable-itron --disable-networking --disable-multiprocessing --enable-rtemsbsp=sun4u
 #../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4u --disable-posix
-#../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4u --enable-tests=samples
-../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4u --enable-tests
+../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4u --enable-tests=samples
+#../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4u --enable-tests
 
 time make -j 4 2> make.error
 
--- rtems/build-b-sun4vcvs.sh
+++ rtems/build-b-sun4vcvs.sh
@@ -4,8 +4,8 @@ cd b-sun4vcvs
 rm * -rf
 #../rtemscvs/configure --target=sparc64-rtems4.10 --disable-posix --disable-itron --disable-networking --disable-multiprocessing --enable-rtemsbsp=sun4v
 #../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4v --disable-posix
-#../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4v --enable-tests=samples
-../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4v --enable-tests
+../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4v --enable-tests=samples
+#../rtemscvs/configure --target=sparc64-rtems4.10 --disable-networking --enable-rtemsbsp=sun4v --enable-tests
 
 time make -j 4 2> make.error
 
