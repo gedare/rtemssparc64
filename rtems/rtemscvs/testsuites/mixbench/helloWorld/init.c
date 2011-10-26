f371cdcb12e0851a0ec4114aa3deb7151c423c8a
eleontie <eleontie@bd9825d0-bf6e-11de-8cd0-f941f5a20a08>
2011-10-26 18:42:54 +0000

    added mixbench

---
+++ rtems/rtemscvs/testsuites/mixbench/helloWorld/init.c
@@ -0,0 +1,131 @@
+/*  Init
+ *
+ *  This routine is the initialization task for this test program.
+ *  It is called from init_exec and has the responsibility for creating
+ *  and starting the tasks that make up the test.  If the time of day
+ *  clock is required for the test, it should also be set to a known
+ *  value by this function.
+ *
+ *  Input parameters:  NONE
+ *
+ *  Output parameters:  NONE
+ *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id: init.c,v 1.17 2009/10/02 11:50:39 joel Exp $
+ */
+
+#define CONFIGURE_INIT
+#include "system.h"
+#include "../../common/magic-instruction.h"
+
+
+rtems_task TaskFunction(
+  rtems_task_argument argument
+)
+
+{
+  int run= 20;
+  while(run){
+	printf( "Hello World %d %d\n", (int)argument, run);
+	run --;
+  }
+  rtems_status_code status = rtems_task_delete( RTEMS_SELF );
+  directive_failed( status, "rtems_task_delete of RTEMS_SELF" );
+}
+
+rtems_task KillTaskFunction(
+  rtems_task_argument argument
+)
+{
+  printf( "DONE !\n");
+  MAGIC_BREAKPOINT;
+  exit(0);
+}
+
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  rtems_time_of_day time;
+  rtems_status_code status;
+
+  puts( "\n\n*** TEST 1 ***" );
+
+  build_time( &time, 12, 31, 1988, 9, 0, 0, 0 );
+  status = rtems_clock_set( &time );
+  directive_failed( status, "rtems_clock_set" );
+
+  Task_name[ 1 ] = rtems_build_name( 'T', 'A', '1', ' ' );
+  Task_name[ 2 ] = rtems_build_name( 'T', 'A', '2', ' ' );
+  Task_name[ 3 ] = rtems_build_name( 'T', 'A', '3', ' ' );
+  Task_name[ 4 ] = rtems_build_name( 'K', 'I', 'L', 'L' );
+
+  status = rtems_task_create(
+     Task_name[ 1 ],
+     1,
+     RTEMS_MINIMUM_STACK_SIZE ,
+     RTEMS_PREEMPT|RTEMS_TIMESLICE,
+     RTEMS_DEFAULT_ATTRIBUTES,
+     &Task_id[ 1 ]
+  );
+  directive_failed( status, "rtems_task_create of TA1" );
+
+  status = rtems_task_create(
+     Task_name[ 2 ],
+     1,
+     RTEMS_MINIMUM_STACK_SIZE ,
+     RTEMS_PREEMPT|RTEMS_TIMESLICE,
+     RTEMS_DEFAULT_ATTRIBUTES,
+     &Task_id[ 2 ]
+  );
+  directive_failed( status, "rtems_task_create of TA2" );
+
+  status = rtems_task_create(
+     Task_name[ 3 ],
+     1,
+     RTEMS_MINIMUM_STACK_SIZE ,
+     RTEMS_PREEMPT|RTEMS_TIMESLICE,
+     RTEMS_DEFAULT_ATTRIBUTES,
+     &Task_id[ 3 ]
+  );
+  directive_failed( status, "rtems_task_create of TA3" );
+
+  status = rtems_task_create(
+     Task_name[ 4 ],
+     2,
+     RTEMS_MINIMUM_STACK_SIZE ,
+     RTEMS_PREEMPT|RTEMS_TIMESLICE,
+     RTEMS_DEFAULT_ATTRIBUTES,
+     &Task_id[ 4 ]
+  );
+  directive_failed( status, "rtems_task_create of KILL" );
+
+  puts( "\n\n*** TEST START *** \n" );
+
+  MAGIC_BREAKPOINT;
+
+  status = rtems_task_start( Task_id[ 1 ], TaskFunction, 1 );
+  directive_failed( status, "rtems_task_start of TA1" );
+
+  status = rtems_task_start( Task_id[ 2 ], TaskFunction, 2 );
+  directive_failed( status, "rtems_task_start of TA2" );
+
+  status = rtems_task_start( Task_id[ 3 ], TaskFunction, 3 );
+  directive_failed( status, "rtems_task_start of TA3" );
+
+  status = rtems_task_start( Task_id[ 4 ], KillTaskFunction, 3 );
+  directive_failed( status, "rtems_task_start of TA4" );
+
+  status = rtems_task_delete( RTEMS_SELF );
+  directive_failed( status, "rtems_task_delete of RTEMS_SELF" );
+
+
+}
+
---
+++ rtems/rtemscvs/testsuites/mixbench/mix01/system.h
@@ -0,0 +1,79 @@
+/*  system.h
+ *
+ *  This include file contains information that is included in every
+ *  function in the test set.
+ *
+ *	
+ */
+
+
+#include <tmacros.h>
+
+#include "FileSystemImage.h"
+#include <rtems/untar.h>
+
+/* functions */
+
+rtems_task Init(
+  rtems_task_argument argument
+);
+
+rtems_task Task_1_through_3(
+  rtems_task_argument argument
+);
+
+void test_switch (
+  Thread_Control * a,
+  Thread_Control * b
+);
+
+bool test_create(Thread_Control * executing,  Thread_Control * creating);
+
+
+#define CONFIGURE_INITIAL_EXTENSIONS \
+  { \
+    test_create,             /* create  */ \
+    NULL,                    /* start   */ \
+    NULL,                    /* restart */ \
+    NULL,                    /* delete  */ \
+    test_switch,             /* switch  */ \
+    NULL,                    /* begin   */ \
+    NULL,                    /* exitted */ \
+    NULL          			 /* fatal   */ \
+  }
+
+
+
+/* configuration information */
+//#define CONFIGURE_ZERO_WORKSPACE_AUTOMATICALLY 1
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+/* filesystem */
+#define CONFIGURE_USE_IMFS_AS_BASE_FILESYSTEM
+#define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 40
+#define CONFIGURE_IMFS_MEMFILE_BYTES_PER_BLOCK 512
+
+/* tasks */
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+#define CONFIGURE_TICKS_PER_TIMESLICE       1
+#define CONFIGURE_MICROSECONDS_PER_TICK		RTEMS_MILLISECONDS_TO_MICROSECONDS(5)
+
+
+
+#define CONFIGURE_INIT_TASK_STACK_SIZE 20*RTEMS_MINIMUM_STACK_SIZE
+
+#define CONFIGURE_EXTRA_TASK_STACKS         (20 * RTEMS_MINIMUM_STACK_SIZE)
+#define CONFIGURE_MAXIMUM_TASKS             6
+
+#include <rtems/confdefs.h>
+
+/* global variables */
+
+TEST_EXTERN rtems_id   Task_id[ 5 ];         /* array of task ids */
+TEST_EXTERN rtems_name Task_name[ 5 ];       /* array of task names */
+
+/* end of include file */
+
+
